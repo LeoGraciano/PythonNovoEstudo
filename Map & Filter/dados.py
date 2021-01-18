bcfc46f913a7f2bf1199cbff7319700314775134
Leonardo <leonardoferreiragraciano@gmail.com>
2021-01-18 09:32:31 -0300

    map e filter

---
+++ Map & Filter/dados.py
@@ -0,0 +1,27 @@
+produtos = [
+    {'nome': 'p1', 'preço': 13},
+    {'nome': 'p2', 'preço': 1},
+    {'nome': 'p3', 'preço': 25},
+    {'nome': 'p4', 'preço': 19},
+    {'nome': 'p5', 'preço': 13.90},
+    {'nome': 'p6', 'preço': 5},
+    {'nome': 'p7', 'preço': 30},
+    {'nome': 'p8', 'preço': 20},
+    {'nome': 'p9', 'preço': 10},
+    {'nome': 'p10', 'preço': 8},
+
+]
+pessoas = [
+    {'nome': "Leonardo", 'idade': 35},
+    {'nome': "Jessica", 'idade': 28},
+    {'nome': "Paulo", 'idade': 27},
+    {'nome': "Cida", 'idade': 55},
+    {'nome': "Maria", 'idade': 20},
+    {'nome': "João", 'idade': 25},
+    {'nome': "Pedro", 'idade': 27},
+    {'nome': "Felipe", 'idade': 39},
+    {'nome': "Ana", 'idade': 20},
+    {'nome': "Thiago", 'idade': 16},
+]
+
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
